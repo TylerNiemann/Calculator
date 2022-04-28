45bb25f2d677d6f6779e6c80db788a37eea6e34e
Tyler Niemann <tntman98@gmail.com>
2022-04-28 09:58:40 -0700

    add basic math operator functions

---
+++ calc.js
@@ -0,0 +1,12 @@
+let add = (inputOne,inputTwo) => inputOne + inputTwo;
+
+let subtract = (inputOne,inputTwo) => inputOne - inputTwo;
+
+let multiply= (inputOne,inputTwo) => inputOne * inputTwo;
+
+let divide = (inputOne,inputTwo) => inputOne / inputTwo;
+
+console.log(add(1,2));
+console.log(subtract(4,12));
+console.log(multiply(4,12));
+console.log(divide(12,4));
